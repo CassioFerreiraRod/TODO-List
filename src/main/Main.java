8736e3d7fedd5a2680b21ca29eda583c42d1b94a
Cassio Ferreira Rodrigues <cassio1215@hotmail.com>
2024-02-17 15:44:13 -0300

    Menu adicionado ao main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,14 @@
 package main;
 
+import menu.Menu;
+import tarefas.ListaDeTarefas;
+
+
 public class Main {
+
     public static void main(String[] args) {
+        Menu.exibirMenu();
 
     }
+
 }
