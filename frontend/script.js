10926715fcaf181dadccd1694930df47178d8a40
Cassio Ferreira Rodrigues <cassio1215@hotmail.com>
2024-03-05 22:02:37 -0300

    Adicionando funcionalidade de excluir tarefa

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,12 @@
+function limparcampos() {
+    document.getElementById('nome').value = '';
+    document.getElementById('descricao').value = '';
+    document.getElementById('data').value = '';
+    document.getElementById('prioridade').value = '';
+    document.getElementById('categoria').value = '';
+    document.getElementById('status').value = '';
+}
+
 document.getElementById('botaoCriar').onclick = function () {
     let nomeTarefa = document.getElementById('nome').value
     let descricaoTarefa = document.getElementById('descricao').value
@@ -13,9 +22,15 @@ document.getElementById('botaoCriar').onclick = function () {
                           <td>${prioridadeTarefa}</td>
                           <td>${categoriaTarefa}</td>
                           <td>${statusTarefa}</td>
+                          <td><button type="button" class="btn-close" aria-label="Close" id="excluir"  onclick="excluirLinha(this)"></button></td>
                       </tr>`
+        limparcampos()
     } else {
         alert('Preencha todos os campos.')
     }
+}
 
+function excluirLinha(botaoExcluir) {
+    var linha = botaoExcluir.parentNode.parentNode; // Obtenha a linha associada ao botão
+    linha.parentNode.removeChild(linha); // Remova a linha da tabela
 }
